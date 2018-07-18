ec39880f1e6e3eac4dcf410af8791e49859de851
YUMEE <yumeehk@hotmail.com>
2018-07-18 17:35:36 -0400

    Tic Tac Toe works. Need to add function to reset properly after the game is over

--- js/main.js
+++ js/main.js
@@ -9,21 +9,14 @@ var winGame = [
   [7,8,9],
   [1,4,7],
   [1,5,9],
-  [2,5,7],
+  [2,5,8],
   [3,6,9],
   [3,5,7],
 ]
 var game = 0;
 
-// alternate between O and X depending on the game round. Start with O.
-function player() {
-  if (game % 2 == 0) {
-    return 'O';}
-  else {
-    return 'X';}
-  }
 
-// Game adds 1 as a player plays.
+// Game count adds 1 as a player plays.
 squares.forEach(function(square){
   square.addEventListener('click', function() {
     if (square.innerText == '') {
@@ -32,11 +25,54 @@ squares.forEach(function(square){
     else {
       square.disabled = true;
     }
-    game += 1;
+    checkWin();
+    game+=1;
   });
 });
 
+// alternate between O and X depending on the game round. Start with O.
+function player() {
+  if (game % 2 == 0) {
+    return 'O';}
+    else {
+      return 'X';}
+    }
 
 
+// if winGame matches with location of X and Os, stop the game
+var winLine = document.querySelector('.winning-line');
+
+function checkWin(){
+  winGame.forEach(function(set) {
+    var one = squares[set[0]-1].innerText
+    var two = squares[set[1]-1].innerText
+    var three = squares[set[2]-1].innerText
+
+    if (one != '' && one === two && one === three) {
+      winLine.innerHTML = "Player "+ "'" + one + "' wins!";
+      endGame();
+
+    };
+    });
+};
+
+function endGame() {
+
+}
+
+function resetGame() {
+  winLine.innerText = '';
+  squares.forEach(function(square) {
+    square.innerText = ''
+  })
+
+};
+
+// reset button
+var reset = document.querySelector('.reset')
+
+reset.addEventListener('click', function(){
+  resetGame();
+})
 
 });
